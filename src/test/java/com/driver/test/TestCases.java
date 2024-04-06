d6f092fccbb30a3e44d6d578a51bd0cf2557dba9
Arvapalli-Ramprasad <155809700+Arvapalli-Ramprasad@users.noreply.github.com>
2024-04-06 13:09:11 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,7 +2,14 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
+import com.driver.RWOnly;
+import org.junit.Test;
 
 public class TestCases {
-    
+     @Test
+    public void testGetterSetter() {
+        RWOnly obj = new RWOnly();
+        obj.setName("TestName");
+        assertEquals("TestName", obj.getName());
+    }
 }
